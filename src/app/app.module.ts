fb7451c03b01cb5d2049d4da5cf8991acc12bff0
srivindhya <srivindhya.kasaraneni@evry.com>
2018-08-01 11:22:28 +0530

    Creating Navigation using the Angular Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -8,8 +8,14 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import {ProductService} from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
-import { ProductListComponent } from './product-list/product-list.component'
+import { ProductListComponent } from './product-list/product-list.component';
+import{RouterModule, Routes} from '@angular/router'
 
+const appRoutes:Routes=[
+ { path:'products', component: ProductListComponent},
+ { path:'product/:id',component: ProductPageComponent},
+ { path:'', redirectTo:'products', pathMatch:'full'  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +27,8 @@ import { ProductListComponent } from './product-list/product-list.component'
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
